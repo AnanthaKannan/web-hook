8e479bbc0bc22cf522c7f45f118237f5e8e82914
AnanthaKannan <sreeananthakannan@gmail.com>
2020-04-06 11:12:48 +0530

    'updated'

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # web-hook
+
+(webhook reference)[https://cloud.google.com/dialogflow/docs/fulfillment-webhook]
+
+### https://stark-river-21682.herokuapp.com/
+### https://stark-river-21682.herokuapp.com/webhook
+### Heroku
+###    configuration
+    const port = process.env.PORT || 3020;
+    "start": "node app.js"
+
+    1. heroku login
+    2. heroku create
+    3. git push heroku master
--- app.js
+++ app.js
@@ -4,22 +4,28 @@ const cors = require('cors')
 const app = express();
 app.use(cors())
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3020;
 app.use(express.json());
 
 app.get('/', function (req, res) {
     res.send('Sucessfully wroking..')
 });
 
-app.post('/dialog_flow', async(req, res) => {
-    const { text } = req.body;
-    return text;
-    // dialog_flow(text).then((data) =>{
-    //     return res.status(200).json({status:200, data:data})
-    // })
-    // .catch((error) =>{
-    //     return res.status(400).json({status:400, error:error})
-    // })
+app.post('/webhook', async(req, res) => {
+    console.log('webhook_api_called');
+    // const { text } = req.body;
+    if(!req.body) res.sendStatus(400)
+    res.setHeader('Content-Type', 'application/json')
+    const queryResult = req.body.queryResult;
+    // const parameters = req.body.queryResult.parameters;
+    console.log(queryResult)
+    const responseObj = {
+        "fulfillmentText": "This is a text response",
+        "fulfillmentMessages":[{"text": { "text":"this response came from webhook" } }],
+        "source":""
+    }
+    console.log('responseObj', responseObj)
+    return res.json(responseObj);
 })
 
 app.listen(port, () => {
